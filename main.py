f664023576fbefab2d6f2ad780004ab6e862090b
ACuteWoof <63534975+SutharshanVithushan@users.noreply.github.com>
2021-02-04 13:35:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
-#WOOF WARSHIP DISCORDS BOT
+# WOOF WARSHIP DISCORDS BOT
 
-#imports
-import discord
+# imports
+# standard library imports
+import asyncio
 import random
+import re
+import string
 import time
-import json
-import re, string
+
+# third-part imports
+import discord
 from discord.ext import commands
 from pretty_help import PrettyHelp
 
-#moderation category
+#intents
+intents = discord.Intents(messages=True, guilds=True)
+intents.reactions = True # to be able to manage reactions
+intents.members = True # to be able to interact with member updates
+intents.typing = False # to disable typing
+intents.presences = False #to disable presences
+
+
+#Moderation category
 class Moderation(commands.Cog, description="Commands for Moderation. Prefix = $"):
+
+    #silence channel
+    @commands.has_permissions(administrator=True) # see if command author has admin perms
+    @commands.command(name='hush', help='Silence a text channel')
+    async def hush(self, ctx):
+        role_members = discord.utils.get(ctx.guild.roles, name='Not A Woof (Yet)')
+        await ctx.channel.set_permissions(role_members, send_messages=False)
+
+
     #mute command
-    @commands.has_permissions(kick_members=True)
+    @commands.has_permissions(kick_members=True) # see if command author has kick perms
     @commands.command(name='mute', help="Disables the access of a user to send messages")
     async def mute(self, ctx, member: discord.Member, *, reason="No reason provided", timein = 30000):
         role_muted = discord.utils.get(ctx.guild.roles, name='Muted')
@@ -30,7 +51,7 @@ class Moderation(commands.Cog, description="Commands for Moderation. Prefix = $"
 
 
     #unmute command
-    @commands.has_permissions(kick_members=True)
+    @commands.has_permissions(kick_members=True) # see if command author has kick perms
     @commands.command(name='unmute', help="Enables the access of a user to send messages")
     async def unmute(self, ctx, member: discord.Member):
         role_muted = discord.utils.get(ctx.guild.roles, name='Muted')
@@ -48,21 +69,20 @@ class Moderation(commands.Cog, description="Commands for Moderation. Prefix = $"
 
 
     #kick command
-    @commands.has_permissions(kick_members=True)
+    @commands.has_permissions(kick_members=True) # see if command author has kick perms
     @commands.command(name='kick', help='Kicks a user from this server')
     async def kick(self, ctx, user: discord.Member, *, reason="No reason provided"):
         server_name = ctx.message.guild.name
         kickmsg = discord.Embed(title=f"**:guide_dog: Kicked {user.name}!**", description=f"**Reason: {reason}**\n**By: {ctx.author.mention}**").set_image(url="https://i.pinimg.com/originals/c0/f1/f8/c0f1f8bfb261a92525cb81501fe82903.gif")
         kickdm = discord.Embed(title=f"You Were Kicked From {server_name}! :door:", description=f"Reason: {reason}\nBy: {ctx.author.mention}").set_image(url="https://emoji.gg/assets/emoji/7886_jimmy.png")
 
-        await ctx.message.delete()
         await ctx.channel.send(embed=kickmsg)
         await user.send(embed=kickdm)
         await user.kick(reason=reason)
 
 
     #ban command
-    @commands.has_permissions(ban_members=True)
+    @commands.has_permissions(ban_members=True) # see if command author has ban perms
     @commands.command(name='ban', help='Bans a user from this server')
     async def ban(self, ctx, user: discord.Member, *, reason="No reason provided"):
         server_name = ctx.message.guild.name
@@ -74,12 +94,13 @@ class Moderation(commands.Cog, description="Commands for Moderation. Prefix = $"
         await user.send(embed=bandm)
         await user.ban(reason=reason)
 
+
     #clear command
-    @commands.has_permissions(administrator=True)
+    @commands.has_permissions(administrator=True) # see if command author has admin perms
     @commands.command(name='clear', help='Clears a set of messages')
     async def clear(self, ctx, amount = 1):
-        await ctx.channel.purge(limit=amount)
-        embed_msg = discord.Embed(title=f":guide_dog: Cleared {amount} Messages!").set_image(url="https://media1.tenor.com/images/db3f81f2fe5f1513dae0027453064c2c/tenor.gif?itemid=17051644")
+        await ctx.channel.purge(limit=amount+1)
+        embed_msg = discord.Embed(title=f":guide_dog: Cleared {amount} Messages!").set_image(url="https://tenor.com/view/shibe-snow-shibe-shaking-shiba-inu-shiba-inu-cute-shiba-gif-19856803")
 
         await ctx.channel.send(embed=embed_msg)
 
@@ -87,7 +108,8 @@ class Moderation(commands.Cog, description="Commands for Moderation. Prefix = $"
         await ctx.channel.purge(limit=1)
 
 
-bot = commands.Bot(command_prefix=commands.when_mentioned_or('$'), help_command=PrettyHelp(color=discord.Color.green()))
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('$'),intents=intents ,help_command=PrettyHelp(color=discord.Color.green()))
+
 
 def isolate(s):
     l = re.findall(r"[\w']+", s)
@@ -103,16 +125,26 @@ async def on_ready():
 
 
 @bot.event
+async def on_member_join(member):
+    role = discord.utils.get(member.guild.roles, name='Not A Woof (Yet)')
+    await member.add_roles(role)
+
+
+@bot.event
 async def on_message(message):
+
     print(str(message.author) + ": " + str(message.content))
 
     if message.author == bot.user:
         return
 
-    hello_phrases = ['hello', 'hi', 'Hi', 'Hey', 'hey', 'Hello', 'Konichiwa']
+    hello_phrases = ['hello', 'hi', 'Hi', 'Hey', 'hey', 'Hello']
 
     if any(word in hello_phrases for word in isolate(message.content)):
         await message.channel.send('Hello! :wave:')
+
+    if 'konichiwa' in isolate(message.content):
+        await message.channel.send('Konichiwa! :wave:')
 
     if message.content == "ping" or message.content == "ping":
         await message.channel.send("Pong :ping_pong:")
